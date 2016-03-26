00105cd7bc4f5a853bd1bc47fd8977c9a48be9c4
ShadowBan <chris.kincanon@gmail.com>
2016-03-25 17:24:42 -0700

    master: adding bulma npm packages and starting to play with layout

--- web/views/layout_view.ex
+++ web/views/layout_view.ex
@@ -1,3 +1,28 @@
 defmodule HealthInspector.LayoutView do
   use HealthInspector.Web, :view
+  use Phoenix.HTML
+  
+   @doc """
+  Generates tag for inlined form input errors.
+  """
+  def msg_tag(conn, type) do
+    msg = nil 
+    type_class = ""
+    case type do
+      :info ->
+        msg = get_flash(conn, :info)
+        type_class = "is-info"
+      :error ->
+        msg = get_flash(conn, :error)
+        type_class = "is-danger"
+    end
+    if msg do
+      content_tag(:div, class: "notification " <> type_class) do
+        [
+          content_tag(:button,"", class: "delete"),
+          content_tag(:span, msg)
+        ]
+      end
+    end
+  end 
 end
